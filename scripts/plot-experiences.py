eba60ec7e2f60e40b1fdf9cb89fd3f0dedc16821
Davide Madrisan <davide.madrisan@gmail.com>
2017-01-08 21:15:13 +0200

    plot_experiences.py: rewrite it using a more functional style
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- scripts/plot-experiences.py
+++ scripts/plot-experiences.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # Create a scatter plot for visualizing all my job experences and
 # programming activities.
@@ -13,58 +13,67 @@ import matplotlib.pyplot as plt
 import datetime
 import numpy as np
 
-from experiences import jobs
-
 def mnorm(month):
     """Normalize the month number."""
     assert(month in range(13))
     return (month - 1) / 12.0
 
-def jcolor(index, upper):
-    """Return the plotter color associated to a job."""
+def ecolor(index, upper):
+    """Return the plotter color associated to an experience."""
     assert(index in range(upper))
     return plt.cm.Paired(np.linspace(0, 1, upper))[index]
 
-def jindex(job):
-    """Return the index of a 'job' in the vector 'jobs'."""
-    return jobs.index(job)
+def parser(source):
+    """Parse the history file and returns a list containing the
+       experiences type and map of all the experiences."""
 
-def jparser(history, bullet_factor=2.2):
-    """Parse the jobs history and initialize date, shape, jobtype, and color
-       variables."""
+    skip_line = lambda line: line.startswith('#') or not line.strip()
+    date = lambda sy, sm, ey, em: \
+        (int(ey) + mnorm(int(em)) + int(sy) + mnorm(int(sm))) / 2.0
+    months = lambda sy, sm, ey, em: \
+        12*(int(ey) - int(sy)) + int(em) - int(sm)
 
-    year, months, shape, jobtype, colors = ([] for _ in xrange(5))
-    for job in history:
-        ystart = job['year_start'][0] + mnorm(job['year_start'][1])
-        yend = job['year_end'][0] + mnorm(job['year_end'][1])
-        delta = yend - ystart
-        assert(delta >= 0)
+    def row_iter(file_obj):
+        return (nextline.strip().split(',')
+            for nextline in file_obj if not skip_line(nextline))
 
-        yhalf = ystart + delta / 2.0
-        year.append(yhalf)
+    def my_experiences(experience_iter):
+        return tuple((
+            date(sy, sm, ey, em), months(sy, sm, ey, em), e.strip())
+            for sy, sm, ey, em, e in experience_iter)
 
-        job_months = 12.0 * delta + 1
-        months.append(job_months)
+    with open(source, "r") as data:
+        history = my_experiences(row_iter(data))
+        expr_types = sorted(set([expr[2] for expr in history]))
 
-        shape.append(bullet_factor * job_months ** 2)
-        job_index = jindex(job['type'])
-        jobtype.append(job_index)
-        colors.append(jcolor(job_index, len(jobs)))
+        index = lambda e: expr_types.index(e)
+        color = lambda e: ecolor(index(e), len(expr_types))
+        expr_map = map(lambda e:
+            (e[0], e[1], index(e[2]), color(e[2])), history)
 
-    return year, shape, jobtype, colors
+    return expr_types, expr_map
 
-def make_jobs_plot((year, shape, jobtype, colors)):
+def make_jobs_plot(experiences, area_adj=0.84):
     """Create the jobs history plot and legend"""
 
     def textlabel(sp, x, y, text, rotation='horizontal'):
-        """Add a floating text to a subplot sp"""
+        """Add a floating text to a subplot 'sp'"""
         sp.text(x, y, text, style='italic', color='gray', fontsize=10,
                 rotation=rotation)
+
+    expr_types, expr_map = experiences
+    years, area, experiences, colors = ([] for _ in range(4))
+
+    for e in expr_map:
+        years.append(e[0])
+        area.append(np.pi * (area_adj * e[1])**2)
+        experiences.append(e[2])
+        colors.append(e[3])
 
     fig = plt.figure(1)
 
     ax = fig.add_subplot(111)
-    ax.scatter(x=year, y=jobtype, s=shape, c=colors, alpha=0.5)
+    ax.scatter(x=years, y=experiences, s=area, c=colors, alpha=0.5)
 
     # Get the current date
     now = datetime.datetime.now()
@@ -74,7 +83,7 @@ def make_jobs_plot((year, shape, jobtype, colors)):
     ax.set_ylabel('Job Typology')
     ax.set_title('Job History')
     ax.set_xlim([1995, now.year + 3])
-    ax.set_ylim([-1, len(jobs) + 1])
+    ax.set_ylim([-1, len(expr_types) + 1])
 
     # Add grid - fixme: yticks[] has to be manually adjusted
     yticks = [8, 11, 12]
@@ -95,18 +104,21 @@ def make_jobs_plot((year, shape, jobtype, colors)):
     textlabel(ax, xnow+0.05, 0.5, "%s.%s" % (now.year, now.month), rotation=45)
 
     # Add a legend
-    handles = [mpatches.Patch(color=jcolor(jindex(job), len(jobs)),
-                              label=job, alpha=0.8) for job in jobs]
-    lgd = ax.legend(reversed(handles), reversed(jobs),
+    handles = \
+        list(mpatches.Patch(color=ecolor(expr_types.index(e), len(expr_types)),
+             label=e, alpha=0.8) for e in expr_types)
+    lgd = ax.legend(reversed(handles), reversed(expr_types),
                     loc='upper right', bbox_to_anchor=(1.55, 0.8),
                     prop={'size': 12})
 
     return fig, lgd
 
+def main():
+    source = 'experiences.csv'
+    picture = '../images/experiences.png'
 
-from experiences import history
-fig, legend = make_jobs_plot(jparser(history))
+    fig, legend = make_jobs_plot(parser(source))
+    fig.savefig(picture, bbox_extra_artists=(legend,), bbox_inches='tight')
 
-# Save the plot on a png file
-filename = '../images/experiences.png'
-fig.savefig(filename, bbox_extra_artists=(legend,), bbox_inches='tight')
+if __name__ == '__main__':
+    main()
