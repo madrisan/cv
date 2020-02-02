b35b16110ff3bdf8ac9f740bddb3bba04f8d6717
Davide Madrisan <davide.madrisan@gmail.com>
2020-02-02 17:22:14 +0100

    cv: document the recent moocs on environmental sciences
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- scripts/plot-experiences.py
+++ scripts/plot-experiences.py
@@ -214,8 +214,8 @@ def make_jobs_plot(experiences, area_adj):
         1,
         # 2019   -    -      -        -   -       -         1    -        -
         1,
-        # 2020   -    -      -        -   -       -         -    -        1
-        1]
+        # 2020   -    -      -        -   -       -         -    -        2
+        3]
     ax2.bar(years, courses, width=bar_width, color=bar_color, alpha=0.2)
     ax2.set_yticks([])
 
